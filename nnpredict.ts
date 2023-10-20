fe8a640687b357b77fec6a19febf248af7207ecd
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-10-20 17:01:41 +0200

    Create nnpredict.ts

---
+++ nnpredict.ts
@@ -0,0 +1,32 @@
+/**
+ * enum for activation function
+ */
+
+enum activationFunctionType{
+	 None=0,
+	 RELU=1,
+	 SIGMOID=2,
+     SOFTMAX=3,
+     TANHYP=4
+}
+
+//% weight=70 icon="\u237E" color=#74DF00 block="AI"
+namespace nnpredict {
+
+    //% weight=90 
+    //% blockId=nntest_fcnnfromjson
+    //% block="Json Brain|string %json"
+    //% shim=nntest::fcnnfromjson
+    export function fcnnfromjson(json: string): void {
+    	basic.showString("sim:json")
+    }
+
+    //% weight=50 
+    //% blockId=nntest_predict
+    //% block="Predict|number[] %input|number[] %output"
+    //% shim=nntest::predict
+    export function predict(input: number[], output: number[]): void {
+    	basic.showString("sim-predict")
+    }
+
+ }
